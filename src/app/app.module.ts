8f7e48f7b29dd2b1b6ad707788c3d4de9aef03ec
Uri <uri@vir-tec.net>
2018-07-04 19:29:13 +0300

    hone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -9,6 +10,12 @@ import { YieldComponent } from './yield/yield.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+import localeHe from '@angular/common/locales/he';
+import localHeExtra from '@angular/common/locales/extra/he';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeHe, 'he-IL', localHeExtra);
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -24,13 +31,14 @@ const routes: Routes = [
     PortfolioComponent,
     YieldComponent,
     TransactionComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ { provide: LOCALE_ID, useValue: 'he' } ],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeComponent]
 })
 export class AppModule { }
